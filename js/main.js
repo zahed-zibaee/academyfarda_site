4ed8c7270bba605929c62db377f18d6607bfbc52
zahed zibaee <zahed.zibaee2@gmail.com>
2020-04-11 14:28:41 +0430

    video popup

--- js/main.js
+++ js/main.js
@@ -417,6 +417,18 @@ $(document).ready(function() {
 		time: 1500
 	});
     
+    /*----------------------------------------------------*/
+    /*  Video Popup
+    /*----------------------------------------------------*/
+    $('.popup-video').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+    
+        fixedContentPos: false
+      });
 
 
 });
